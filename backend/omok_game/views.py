532ef77d5f6b0354ead3152d3829a2efdff4f4ff
정진권 <jjk8060@naver.com>
2020-09-24 14:05:09 +0900

    S03P23A205-14 VueConnectIssueClear

--- backend/omok_game/views.py
+++ backend/omok_game/views.py
@@ -3,9 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['POST'])
 def test(request):
-    board = request.data
+    print('clear', reques.data['board'])
+    board = request.data['board']
     ch = 0
     for i in range(19):
         for j in range(19):
