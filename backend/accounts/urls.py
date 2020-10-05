a05c4daf33632f0de07975e06b3782ea3560b1a3
정진권 <jjk8060@naver.com>
2020-10-05 10:27:23 +0900

    MergeLast

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,10 +5,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('emailAuth/', views.emailAuth),
-<<<<<<< HEAD
     path('login/kakao/', views.kakao_login, name='kakao_login'),
     path('login/kakao/callback/', views.kakao_callback, name='kakao_callback'),
     path('', include('django.contrib.auth.urls')),
-=======
->>>>>>> 83c3fe39708e1ae0de4d241974885846ee8a2f50
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.core.mail import EmailMessage
