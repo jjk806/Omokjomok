baae278225e8763748a71a70e44d3382e1821cf5
정진권 <jjk8060@naver.com>
2020-10-08 09:54:22 +0900

    S03P23A205-96 RecodeDB

--- backend/omok_game/serializers.py
+++ backend/omok_game/serializers.py
@@ -7,3 +7,9 @@ class TrickSolvingSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrickSolving
         fields = '__all__'
+
+class RecodeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recode
+        fields = '__all__'
